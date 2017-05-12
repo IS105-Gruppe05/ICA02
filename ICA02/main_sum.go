7de5e080fee82ff437135efc8ffe03546c652820
Mats Skjaervik <matses15@student.uia.no>
2017-05-12 15:43:36 +0200

    main_sum.go ferdig og klar for innlevering.

--- ICA02/main_sum.go
+++ ICA02/main_sum.go
@@ -1,20 +1,18 @@
 package main
 
-
 import "fmt"
 import "os"
 import "strconv"
+
 //import "./log"
 import "bufio"
 import "./sum"
-
-
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var text string
 	var text2 string
-	fmt.Println("Skriv inn to tall for å summere, skriv q for å slutte")
+	fmt.Println("Skriv inn to tall for å summere, skriv q i begge felter for å avslutte")
 
 	for text != "q" {
 		fmt.Print("Tall nummer 1: ")
@@ -25,20 +23,17 @@ func main() {
 		text2 = scanner.Text()
 		if text != "q" {
 
-			tall1, err := strconv.ParseInt(text,10, 64)
+			tall1, err := strconv.ParseInt(text, 10, 64)
 			if err != nil {
-				fmt.Println(err)
-				fmt.Println("Tallet kan ikke vere: " + text)
+				fmt.Println("Tallet kan ikke være: " + text + ", bruk int64 verdi.")
+			} else {
+				tall2, err := strconv.ParseInt(text2, 10, 64)
+				if err != nil {
+					fmt.Println("Tallet kan ikke være: " + text2 + ", bruk int64 verdi.")
+				} else {
+					fmt.Println(text+" + "+text2+" =", sum.SumInt64(tall1, tall2))
+				}
 			}
-			tall2, err := strconv.ParseInt(text2,10, 64)
-			if err != nil {
-				fmt.Println(err)
-				fmt.Println("Basen kan ikke vere: " + text2)
-			}
-
-
-			fmt.Println(text + " + " + text2 + " =", sum.SumInt64(tall1, tall2))
-
 		}
 	}
 }
